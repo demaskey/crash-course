2ac501112c553a5ab2c97a329584741e4872afad
Christoph DeMaskey <christoph.demaskey@gmail.com>
2021-12-20 13:53:57 -0500

    Add react-router-dom. Try loading routes.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {lazy, Suspense} from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {Route, Switch} from 'react-router-dom';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Home = lazy(()=>import('./Home/Home'));
+
+export default function Routes() {
+  return (
+    <Suspense fallback={<p>Loading</p>}>
+      <Switch>
+        <Route exact path={'/'}>
+          <Home />
+        </Route>
+      </Switch>
+    </Suspense>
+  );
+}
+
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
