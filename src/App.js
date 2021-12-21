300892abc4e1cb8e7f76e121af08807eb42e7cf2
Christoph DeMaskey <christoph.demaskey@gmail.com>
2021-12-21 10:33:20 -0500

    Fix src/index.js and src/app.js so the content renders. Add routes/index.js to split the code out and make it more maintainable.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import AppRoutes from 'routes';
+import React, {lazy, Suspense} from 'react';
+import Routes from 'routes';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <div role="main" id="Content">
-        <AppRoutes />
-      </div>
+      <Router>
+        <div role="main" id="Content">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+          <hr />
+          <Routes />
+        </div>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
-import React, {lazy, Suspense} from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import {Route, Routes} from 'react-router-dom';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const Home = lazy(()=>import('./routes/Home/Home'));
-const About = lazy(()=>import('./routes/About/About'));
-
-export default function AppRoutes() {
-  return (
-    <Suspense fallback={<p>Loading</p>}>
-      <Routes>
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/about'} element={<About />} />
-      </Routes>
-    </Suspense>
-  );
-}
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
